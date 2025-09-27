0c248d94952c905ce80de0dcb2da2766ad2904b5
LageTx <matheuslage070@gmail.com>
2025-09-27 15:09:58 -0300

    criação estrutura json

---
+++ public/app.js
@@ -0,0 +1,35 @@
+const clubes = [
+  {
+    "id": 1,
+    "nome": "Flamengo",
+    "escudo": "./images/escudos/Flamengo.png",
+    "descricao_curta": "O Flamengo foi um time muito vencedor na última década, conquistando diversos títulos nacionais e internacionais.",
+    "fundacao": "17 de novembro de 1895",
+    "estadio": "Maracanã",
+    "mascote": "Urubu",
+    "principais_titulos": ["Copa Intercontinental (1981)", "Copa Libertadores (1981, 2019, 2022)", "Campeonato Brasileiro (1980, 1982, 1983, 1992, 2009, 2019, 2020)", "Copa do Brasil (1990, 2006, 2013, 2022, 2024)"],
+    "idolos": ["Zico", "Gabigol", "Arrascaeta"]
+  },
+  {
+    "id": 2,
+    "nome": "Palmeiras",
+    "escudo": "./images/escudos/Palmeiras.png",
+    "descricao_curta": "O Palmeiras brilhou na última década, conquistando títulos nacionais e internacionais, incluindo a Copa Libertadores e o Brasileirão.",
+    "fundacao": "26 de agosto de 1914",
+    "estadio": "Allianz Parque",
+    "mascote": "Periquito e Porco",
+    "principais_titulos": ["Copa Libertadores (1999, 2020, 2021)", "Campeonato Brasileiro (1960, 1967(1), 1967(2), 1969, 1972, 1973, 1993, 1994, 2016, 2018, 2022, 2023)", "Copa do Brasil (1998, 2012, 2015, 2020)"],
+    "idolos": ["Ademir da Guia", "Marcos", "Dudu"]
+  },
+  {
+    "id": 3,
+    "nome": "Grêmio",
+    "escudo": "./images/escudos/Gremio.png",
+    "descricao_curta": "O Grêmio, conhecido como 'Imortal Tricolor', também conquistou títulos importantes como a Copa Libertadores e a Copa do Brasil.",
+    "fundacao": "15 de setembro de 1903",
+    "estadio": "Arena do Grêmio",
+    "mascote": "Mosqueteiro",
+    "principais_titulos": ["Copa Intercontinental (1983)", "Copa Libertadores (1983, 1995, 2017)", "Campeonato Brasileiro (1981, 1996)", "Copa do Brasil (1989, 1994, 1997, 2001, 2016)"],
+    "idolos": ["Renato Gaúcho","Geromel", "Luan"]
+  }
+];
